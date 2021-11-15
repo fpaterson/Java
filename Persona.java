8d6035c843f35820ec98d2b9710369086c465eaf
fpaterson <53309219+fpaterson@users.noreply.github.com>
2021-11-14 22:56:38 -0300

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -2,49 +2,54 @@ package clubSportivoVecinal;
 
 import java.util.ArrayList;
 
-
 public class Persona {
-	
+
 	private String nombre;
 	private int edad;
 	private ArrayList<Persona> amigos;
-	
+
 	public Persona(String nombre, int edad) {
 		this.nombre = nombre;
 		setEdad(edad);
 		this.amigos = new ArrayList<>();
 	}
-	
-	public Persona (String nombre) {
+
+	public Persona(String nombre) {
 		this.nombre = nombre;
 		this.amigos = new ArrayList<>();
 	}
 
 	public String getNombre() {
-		return nombre;
+		String nombreVacio = "-";
+		if (nombre == null)
+			return nombreVacio;
+		else
+			return nombre;
+
 	}
 
-	public ArrayList<Persona> getAmigos() {		
+	public ArrayList<Persona> getAmigos() {
 		return this.amigos;
 	}
 
 	public void setEdad(int edad) {
 		if (edad > 0 && edad < 130) {
 			this.edad = edad;
-		}
-		else 
+		} else
 			System.out.println("ERROR");
-		
+
 	}
-	
-	public boolean esAmigo (String nombre) {
+
+	public boolean esAmigo(String nombre) {
 		boolean resultado = false;
 		int index;
-		
+
 		for (index = 0; index < this.amigos.size(); index++) {
-				if (this.amigos.get(index).getNombre().equals(nombre))
-					resultado = true;	
-			}
-		
-		return resultado;	
+			if (this.amigos.get(index).getNombre().equals(nombre))
+				resultado = true;
+		}
+
+		return resultado;
 	}
+
+}
